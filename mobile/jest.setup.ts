0d215ca8e5d9ad524df0049ed26c199e2d67afde
jongheecode <jonghee12386@gmail.com>
2026-07-17 04:04:02 +0900

    feat: Expo 프로젝트 스캐폴드 + 네비게이션/SecureStore 의존성, jest 설정

---
+++ mobile/jest.setup.ts
@@ -0,0 +1,5 @@
+jest.mock('expo-secure-store', () => ({
+  getItemAsync: jest.fn(() => Promise.resolve(null)),
+  setItemAsync: jest.fn(() => Promise.resolve()),
+  deleteItemAsync: jest.fn(() => Promise.resolve()),
+}));
